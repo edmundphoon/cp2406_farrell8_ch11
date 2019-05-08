7c7a2b4f33bd0c8e78e7a5fa8c6af2104f7be5f6
edmundpjc <edmundjiancheng.phoon@my.jcu.edu.au>
2019-05-08 16:44:50 +0800

    Array corrected

--- FixDebuggingExercises/FixDebugEleven2.java
+++ FixDebuggingExercises/FixDebugEleven2.java
@@ -9,7 +9,7 @@ public class FixDebugEleven2
       FixDebugOceanLiner bigBoat = new FixDebugOceanLiner();
       ref[0] = redBoat;
       ref[1] = blueBoat;
-      ref[1] = bigBoat;
+      ref[2] = bigBoat;
       for(int x = 0; x < ref.length; ++x)
       {
          ref[x].setPassengers();
